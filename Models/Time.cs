9ba31a174ebb1d7bb2cf0845b92f770f926a242e
matthewmccord <matthewmccord3@gmail.com>
2022-03-17 16:13:15 -0600

    Time and User Model

---
+++ Models/Time.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _2proj2.Models
+{
+    public class Time
+    {
+        public int TimeID { get; set; }
+
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+        public bool Available { get; set; }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _2proj2.Models
+{
+    public class User
+    {
+        [Key]
+        [Required]
+        public int GroupId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public int GroupSize { get; set; }
+        [Required]
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        // add in the connection with the time slot
+        [Required]
+        public int TimeId { get; set; }
+
+
+    }
+}
